4a2f65deeb2a39daa20317765ce6e0eece8ccfa5
unknown <maurot970@gmail.com>
2021-08-11 17:57:16 -0300

    cambio en la base de datos

--- esquema.sql
+++ esquema.sql
@@ -1,12 +1,204 @@
-CREATE TABLE IF NOT EXISTS productos(
-    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    descripcion VARCHAR(1024) NOT NULL,
-    precio DECIMAL(9,2)
-);
-CREATE TABLE IF NOT EXISTS carrito_usuarios(
-    id_sesion VARCHAR(255) NOT NULL,
-    id_producto BIGINT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_producto) REFERENCES productos(id)
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-08-2021 a las 21:33:04
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito_usuarios`
+--
+
+CREATE TABLE `carrito_usuarios` (
+  `id_sesion` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `id_producto` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `carrito_usuarios`
+--
+
+INSERT INTO `carrito_usuarios` (`id_sesion`, `id_producto`) VALUES
+('oac7tdd6lk7jdguo2q0oh0g99h', 1),
+('9i5j76biqk5o1ln04fbpjuavli', 1),
+('nhtgp9893ehdbjolvd4al778c9', 2),
+('msjft9oqc8jdottpi2gjdui2f8', 1),
+('agdfuf7nvt5irht2f8c64oktqi', 1),
+('k9o5ai1eoauuqbuh2epg2l1ikr', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fecha`
+--
+
+CREATE TABLE `fecha` (
+  `id` int(11) NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opciones`
+--
+
+CREATE TABLE `opciones` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `precio` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `opciones`
+--
+
+INSERT INTO `opciones` (`id`, `descripcion`, `precio`) VALUES
+(1, 'Desayuno Gratis', 500),
+(2, 'Paseo express', 1500);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` varchar(1024) COLLATE utf8_spanish_ci NOT NULL,
+  `precio` decimal(9,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `descripcion`, `precio`) VALUES
+(1, 'Ticket para viajar córdoba', 'Con este ticket usted y tu familia podrán disfrutar de las mejores vacaciones de su vida', '8000.00'),
+(2, 'Viaje a Guamini', 'Incluye un pase por guamini, junto a un coordinador', '1500.00'),
+(3, 'Viaje  Buenos Aires', 'visita  a los mejores lugares, paseo por el shopping', '5000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `nacimiento` date NOT NULL,
+  `provincia` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `nacimiento`, `provincia`, `usuario`, `password`) VALUES
+(2, 'toranzo', 'mauro', '2122-12-12', 'Santa Cruz', 'admin@admin.com', '12345'),
+(8, 'asdasd', 'asdad', '2020-12-12', 'Jujuy', 'gatito', '123456'),
+(9, 'hola', 'gatito', '1999-12-12', 'NeuquÃ©n', 'gato', '12345'),
+(10, 'cristiano', 'ronaldo', '1900-02-20', 'NeuquÃ©n', 'elbicho', '12345'),
+(11, 'elperro', 'diego', '1999-12-01', 'NeuquÃ©n', 'guauguau', '12345678'),
+(12, 'Oriana ', 'Toranzo', '1933-12-12', 'Misiones', 'orianat', '12345678');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carrito_usuarios`
+--
+ALTER TABLE `carrito_usuarios`
+  ADD KEY `id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `fecha`
+--
+ALTER TABLE `fecha`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `fecha`
+--
+ALTER TABLE `fecha`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `opciones`
+--
+ALTER TABLE `opciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `carrito_usuarios`
+--
+ALTER TABLE `carrito_usuarios`
+  ADD CONSTRAINT `carrito_usuarios_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
